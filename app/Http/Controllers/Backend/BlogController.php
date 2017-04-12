62e04aee9622f3dc21d0f087349e4b797f524fc1
Daniel Musteata <musteata.daniel@yahoo.com>
2017-04-12 19:47:34 +0300

    Added AdminLTE Dashboard template wit all js css plugins and libraries

--- app/Http/Controllers/Backend/BlogController.php
+++ app/Http/Controllers/Backend/BlogController.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\Controller;
 
 class BlogController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('backend.pages.blog.posts');
+    }
 }
