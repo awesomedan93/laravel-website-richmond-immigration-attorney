378e168089db4a7442ab6286d74c8c300147fb14
Daniel Musteata <musteata.daniel@yahoo.com>
2017-06-28 21:26:51 +0300

    terms and conditions

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::group([
     Route::get('/blog/{slug}', 'Frontend\PostController@show')->name('blog.post');
 
     Route::get('/contact', 'Frontend\PageController@show')->name('pages.contact');
-    Route::get('/terms-and-conditions', 'Frontend\PostController@show')->name('pages.terms_and_conditions');
+    Route::get('/terms-and-conditions', 'Frontend\PageController@show')->name('pages.terms_and_conditions');
 
     Route::post('/contact', ['as' => 'contact_us', 'uses' => 'Frontend\PageController@sendEmail']);
 });
